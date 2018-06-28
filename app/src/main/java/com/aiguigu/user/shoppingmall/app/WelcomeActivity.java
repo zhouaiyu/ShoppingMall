9a4bd1919865b58ff35653aae376d0e4909800c5
zhouaiyu <1729550553@qq.com>
2018-06-28 11:11:43 +0800

    zhouaiyu2

--- app/src/main/java/com/aiguigu/user/shoppingmall/app/WelcomeActivity.java
+++ app/src/main/java/com/aiguigu/user/shoppingmall/app/WelcomeActivity.java
@@ -1,8 +1,34 @@
 package com.aiguigu.user.shoppingmall.app;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
 /**
  * Created by user on 2018/6/26.
  */
 
-public class WelcomeActivity {
-}
+
+    public class WelcomeActivity extends Activity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_welcome);
+            // 两秒钟进入主页面
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    //执行在主线程
+                    // 启动主页面
+                    startActivity(new Intent(WelcomeActivity.this,MainActivity.class));
+                    //关闭当前页面
+                    finish();
+
+                }
+            },2000);
+        }
+    }
+
+
